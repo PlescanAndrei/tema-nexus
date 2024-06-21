d68b4d426db5bb3431d34d24e9c0a8d9e466129a
luku38 <plescan.andrei.cristian@gmail.com>
2024-06-21 20:13:51 +0300

    Repair from reading second to last array to last array max.java

--- max.java
+++ max.java
@@ -1,11 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Max {
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 4, 5, 6};
 ////                 0  1  2  3  4  5
         int max = arr[0];
-        for (int i = 1; i < arr.length-1; i++){
+        for (int i = 1; i < arr.length; i++){
             if (arr[i] > max) {
                 max = arr[i];
             }
